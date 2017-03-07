62b4bcaeea8ed1ede3225be08b4843019c96afc0
wutp <wutongpeng@pansoft.com>
2017-03-07 17:38:33 +0800

    mq

---
+++ src/com/code2017/rpc/rmi/server/HelloInterface.java
@@ -0,0 +1,11 @@
+package com.code2017.rpc.rmi.server;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface HelloInterface extends Remote {
+	/**
+	 * 远程接口方法必须抛出 java.rmi.RemoteException
+	 */
+	public String say() throws RemoteException;
+}
